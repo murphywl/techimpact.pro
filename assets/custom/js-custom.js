fa2be056c792f15711f177727aef4786d5cc1fc9
Andrey Li <murphywl17s@gmail.com>
2024-12-27 14:17:47 +0500

    update icons and main screen

--- assets/custom/js-custom.js
+++ assets/custom/js-custom.js
@@ -8,3 +8,15 @@ cta.addEventListener("click", function() {
 		bar.classList.remove("out");
 	}, 2000000000);
 }, true);
+
+/*
+var cta = document.getElementById("cta");
+var bar = document.getElementById("cookie-banner");
+
+cta.addEventListener("click", function() {
+    bar.classList.add("out"); // Добавляем класс для анимации скрытия
+    setTimeout(function() {
+        bar.remove(); // Удаляем элемент из DOM
+    }, 500); // Указываем время, совпадающее с длительностью анимации в CSS
+}, true);
+*/
